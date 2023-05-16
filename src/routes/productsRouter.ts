633ed9cc41b19cc9a1c7a76c511eb0ea177da86a
EduardoL-s <eduardoluizmm@hotmail.com>
2023-05-16 12:03:30 -0300

    [Requisito 6] Implementação das validações referentes ao endpoint POST da  rota /products

--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import productsControllers from '../controllers/products.controllers';
+import { amountBodyValidations, nameBodyValidations } from '../middlewares/products.middlewares';
 
 const productsRouter = Router();
 
 productsRouter.get('/', productsControllers.getAll);
-productsRouter.post('/', productsControllers.insertProduct);
+productsRouter.post(
+  '/',
+  nameBodyValidations,
+  amountBodyValidations,
+  productsControllers.insertProduct,
+);
 
 export default productsRouter;
